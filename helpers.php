187c5595ff6a2c2cd2dda5dbba950e9872630012
Vincenzo La Rosa <hello@enzolarosa.dev>
2022-05-23 10:22:20 +0200

    Fixed

--- helpers.php
+++ helpers.php
@@ -3,11 +3,12 @@
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Translation\Translator;
+use Illuminate\Support\Facades\Storage;
 
 if (!function_exists('disk')) {
     function disk(?string $name = null): Filesystem
     {
-        return \Illuminate\Support\Facades\Storage::disk($name);
+        return Storage::disk($name);
     }
 }
 
@@ -15,7 +16,7 @@ if (!function_exists('localize')) {
     function localize($key = null, $replace = [], $locale = null): array|string|Translator|Application|null
     {
         $json = 'lang/' . config('app.locale') . '.json';
-        $keys = json_decode(disk('root')->get($json) ?? [], true);
+        $keys = json_decode(disk('root')->get($json) ?? '[]', true);
 
         if (!isset($keys[$key])) {
             dispatch(function () use ($json, $key) {
