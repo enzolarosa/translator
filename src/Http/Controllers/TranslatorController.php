850f863566441496882e0cdbe2aadddc97efc7c5
Vincenzo La Rosa <hello@enzolarosa.dev>
2022-05-23 11:53:36 +0200

    Update

--- src/Http/Controllers/TranslatorController.php
+++ src/Http/Controllers/TranslatorController.php
@@ -9,21 +9,19 @@ class TranslatorController extends Controller
 {
     public function currentLocale(Request $request)
     {
-        return config('app.locale');
+        return response()->json(config('app.locale'));
     }
 
     public function availableLocales(Request $request)
     {
-        return config('translator.supported_language');
+        return response()->json(config('translator.supported_language'));
     }
 
     public function receive(Request $request, string $locale)
     {
-        $keys = collect(
-            json_decode(
-                disk('translator')->get("$locale.json") ?? '[]', true
-            )
-        );
+        $keys = collect(json_decode(
+            disk('translator')->get("$locale.json") ?? '[]', true
+        ));
 
         if ($search = $request->get('search')) {
             $keys = $keys->where($search);
