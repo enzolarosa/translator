c2c833245857efcc315a24d7fabc0c6e3ab5201b
Vincenzo La Rosa <hello@enzolarosa.dev>
2022-05-25 10:57:56 +0200

    Update translator page

--- resources/js/pages/Translator.vue
+++ resources/js/pages/Translator.vue
@@ -1,65 +1,55 @@
 <template>
-    <div>
-        <Head title="Translator"/>
-        <Heading class="mb-6">Translator</Heading>
+  <div>
+    <Head title="Translator"/>
+    <Heading class="mb-6">Translator</Heading>
 
-        <Card class="flex flex-col items-center justify-center" style="min-height: 300px">
-            <button @click="setLocale(locale)" v-for="locale in locales">Load <b>{{ locale }}</b></button>
-            <br/>
-            <button @click="update" class="btn btn-success">Update Translation</button>
+    <Card class="flex flex-col items-center justify-center" style="min-height: 300px">
+      <button @click="setLocale(locale)" v-for="locale in locales">Load <b>{{ locale }}</b></button>
+      <br/>
+      <button @click="update" class="btn btn-success">Update Translation</button>
 
-            <table class="table-auto">
-                <thead>
-                <th>Key</th>
-                <th>Translation</th>
-                </thead>
-                <tbody>
-                <tr v-for="translate in keys" v-bind:key="translate.key">
-                    <td>{{ translate.key }}</td>
-                    <td>
-                        <input type="text" v-model="translate.str">
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </Card>
-    </div>
+      <div class="" v-for="translate in keys" v-bind:key="translate.key">
+        <span>{{ translate.key }}</span>
+        <input type="text" v-model="translate.str">
+      </div>
+    </Card>
+  </div>
 </template>
 
 <script>
 
 export default {
-    data: () => ({
-        search: null,
-    }),
+  data: () => ({
+    search: null,
+  }),
 
-    props: {
-        keys: Array,
-        locales: Array,
-        locale: '',
+  props: {
+    keys: Array,
+    locales: Array,
+    locale: '',
+  },
+
+  mounted() {
+    // console.log(this.keys, this.locale, this.locales)
+  },
+
+  methods: {
+    update() {
+      // console.log("will update", this.keys);
+      Nova.request()
+          .post(`/nova-vendor/translator`, {
+            keys: this.keys,
+            locale: this.locale
+          })
+          .then(response => {
+            window.location = window.location.href;
+          });
     },
 
-    mounted() {
-        console.log(this.keys, this.locale, this.locales)
-    },
-
-    methods: {
-        update() {
-            console.log("will update", this.keys);
-            Nova.request()
-                .post(`/nova-vendor/translator`, {
-                    keys: this.keys,
-                    locale: this.locale
-                })
-                .then(response => {
-                    window.location=window.location.href;
-                });
-        },
-
-        setLocale(locale) {
-            window.location=window.location.href.replace(this.locale, locale);
-        }
+    setLocale(locale) {
+      window.location = window.location.href.replace(this.locale, locale);
     }
+  }
 }
 </script>
 
