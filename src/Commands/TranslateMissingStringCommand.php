70a5e9c73246c15213de105f595308ec126814a4
Vincenzo La Rosa <hello@enzolarosa.dev>
2024-06-19 13:56:36 +0200

    update translate missing string command

--- src/Commands/TranslateMissingStringCommand.php
+++ src/Commands/TranslateMissingStringCommand.php
@@ -26,30 +26,47 @@ class TranslateMissingStringCommand extends Command
 
     protected function handleDefaultDriver(): void
     {
-        $json = config('translator.locale').'.json';
+        $json = config('app.locale') . '.json';
         $keys = json_decode(disk('translator')->get($json) ?? '[]', true);
 
         $targets = config('translator.supported_language', []);
         $count = [];
         foreach ($targets as $target) {
+            if ($target == config('app.locale')) {
+                continue;
+            }
+            $this->info(localize('translator.missing_command.target.title', [
+                'target' => $target,
+            ]));
+
             $targetKeys = json_decode(disk('translator')->get("$target.json") ?? '[]', true);
             $num = 0;
+
+            $bar = $this->output->createProgressBar(count($keys));
+            $bar->start();
+
             foreach ($keys as $key => $string) {
-                if (array_key_exists($key, $targetKeys) && ! $this->force) {
+                if (array_key_exists($key, $targetKeys) && !$this->force) {
                     continue;
                 }
                 $translated = Translator::translate($string, $target, config('app.locale')) ?? $string;
                 $targetKeys[$key] = $translated;
                 $num++;
-                $this->line("[$target] ===> `$string` will be: `$translated`");
+                $bar->advance();
             }
-            $count = [
+            $bar->finish();
+            $this->info(localize('translator.missing_command.target.complete', [
+                'target' => $target,
+            ]));
+
+            $count[] = [
                 'target' => $target,
                 'count' => $num,
             ];
             disk('translator')->put("$target.json", json_encode(array_unique($targetKeys)));
         }
-        $this->line(localize('translator.missing_string.recap'));
+
+        $this->line(localize('translator.missing_command.recap.table.title'));
 
         $this->table([
             'targets', 'count',
